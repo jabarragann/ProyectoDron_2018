e2d2e419470b55fb49171155fc8f41585fe81dd8
Juan Antonio Barragan <jabarragann@unal.edu.co>
2018-01-22 12:12:57 -0500

    Pin out image added FPGA makefile modification

---
+++ MCU/creator/.dep/atmel_twi.o.d
@@ -0,0 +1,35 @@
+build/obj/atmel_twi.o: \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/atmel_twi.c \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/atmel_twi.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/SAM3S.h \
+ /home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stdint.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/stdint.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/newlib.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/sys/config.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/machine/ieeefp.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/sys/features.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/assert.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/atmel_twi.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/SAM3S.h:
+
+/home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stdint.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/stdint.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/newlib.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/sys/config.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/sys/features.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/assert.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h:
---
+++ MCU/creator/.dep/pal_lld.o.d
@@ -0,0 +1,201 @@
+build/obj/pal_lld.o: \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.c \
+ ../third_party/ChibiOS/os/kernel/include/ch.h chconf.h \
+ ../third_party/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h \
+ /home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stddef.h \
+ /home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stdint.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/stdint.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/newlib.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/sys/config.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/machine/ieeefp.h \
+ /home/santi/arm-2013.11/arm-none-eabi/include/sys/features.h \
+ ../third_party/ChibiOS/os/kernel/include/chlists.h \
+ ../third_party/ChibiOS/os/ports/GCC/ARMCMx/chcore.h \
+ ../third_party/ChibiOS/os/ports/GCC/ARMCMx/AT91SAM3S/cmparams.h \
+ ../third_party/ChibiOS/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../third_party/ChibiOS/os/ports/common/ARMCMx/nvic.h \
+ ../third_party/ChibiOS/os/kernel/include/chsys.h \
+ ../third_party/ChibiOS/os/kernel/include/chvt.h \
+ ../third_party/ChibiOS/os/kernel/include/chschd.h \
+ ../third_party/ChibiOS/os/kernel/include/chsem.h \
+ ../third_party/ChibiOS/os/kernel/include/chbsem.h \
+ ../third_party/ChibiOS/os/kernel/include/chmtx.h \
+ ../third_party/ChibiOS/os/kernel/include/chcond.h \
+ ../third_party/ChibiOS/os/kernel/include/chevents.h \
+ ../third_party/ChibiOS/os/kernel/include/chmsg.h \
+ ../third_party/ChibiOS/os/kernel/include/chmboxes.h \
+ ../third_party/ChibiOS/os/kernel/include/chmemcore.h \
+ ../third_party/ChibiOS/os/kernel/include/chheap.h \
+ ../third_party/ChibiOS/os/kernel/include/chmempools.h \
+ ../third_party/ChibiOS/os/kernel/include/chthreads.h \
+ ../third_party/ChibiOS/os/kernel/include/chdynamic.h \
+ ../third_party/ChibiOS/os/kernel/include/chregistry.h \
+ ../third_party/ChibiOS/os/kernel/include/chinline.h \
+ ../third_party/ChibiOS/os/kernel/include/chqueues.h \
+ ../third_party/ChibiOS/os/kernel/include/chstreams.h \
+ ../third_party/ChibiOS/os/kernel/include/chioch.h \
+ ../third_party/ChibiOS/os/kernel/include/chfiles.h \
+ ../third_party/ChibiOS/os/kernel/include/chdebug.h \
+ ../third_party/ChibiOS/os/hal/include/hal.h \
+ ../third_party/ChibiOS/boards/ATMEL_SAM3S_CREATOR/board.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/at91sam3.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/core_cm3.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/system_SAM3.h \
+ halconf.h mcuconf.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/hal_lld.h \
+ ../third_party/ChibiOS/os/hal/include/tm.h \
+ ../third_party/ChibiOS/os/hal/include/pal.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.h \
+ ../third_party/ChibiOS/os/hal/include/adc.h \
+ ../third_party/ChibiOS/os/hal/include/can.h \
+ ../third_party/ChibiOS/os/hal/include/ext.h \
+ ../third_party/ChibiOS/os/hal/include/gpt.h \
+ ../third_party/ChibiOS/os/hal/include/i2c.h \
+ ../third_party/ChibiOS/os/hal/include/icu.h \
+ ../third_party/ChibiOS/os/hal/include/mac.h \
+ ../third_party/ChibiOS/os/hal/include/pwm.h \
+ ../third_party/ChibiOS/os/hal/include/rtc.h \
+ ../third_party/ChibiOS/os/hal/include/serial.h \
+ ../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/serial_lld.h \
+ ../third_party/ChibiOS/os/hal/include/sdc.h \
+ ../third_party/ChibiOS/os/hal/include/spi.h \
+ ../third_party/ChibiOS/os/hal/include/uart.h \
+ ../third_party/ChibiOS/os/hal/include/usb.h \
+ ../third_party/ChibiOS/os/hal/include/mmc_spi.h \
+ ../third_party/ChibiOS/os/hal/include/serial_usb.h
+
+../third_party/ChibiOS/os/kernel/include/ch.h:
+
+chconf.h:
+
+../third_party/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h:
+
+/home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stddef.h:
+
+/home/santi/arm-2013.11/lib/gcc/arm-none-eabi/4.8.1/include/stdint.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/stdint.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/_ansi.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/newlib.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/sys/config.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/santi/arm-2013.11/arm-none-eabi/include/sys/features.h:
+
+../third_party/ChibiOS/os/kernel/include/chlists.h:
+
+../third_party/ChibiOS/os/ports/GCC/ARMCMx/chcore.h:
+
+../third_party/ChibiOS/os/ports/GCC/ARMCMx/AT91SAM3S/cmparams.h:
+
+../third_party/ChibiOS/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../third_party/ChibiOS/os/ports/common/ARMCMx/nvic.h:
+
+../third_party/ChibiOS/os/kernel/include/chsys.h:
+
+../third_party/ChibiOS/os/kernel/include/chvt.h:
+
+../third_party/ChibiOS/os/kernel/include/chschd.h:
+
+../third_party/ChibiOS/os/kernel/include/chsem.h:
+
+../third_party/ChibiOS/os/kernel/include/chbsem.h:
+
+../third_party/ChibiOS/os/kernel/include/chmtx.h:
+
+../third_party/ChibiOS/os/kernel/include/chcond.h:
+
+../third_party/ChibiOS/os/kernel/include/chevents.h:
+
+../third_party/ChibiOS/os/kernel/include/chmsg.h:
+
+../third_party/ChibiOS/os/kernel/include/chmboxes.h:
+
+../third_party/ChibiOS/os/kernel/include/chmemcore.h:
+
+../third_party/ChibiOS/os/kernel/include/chheap.h:
+
+../third_party/ChibiOS/os/kernel/include/chmempools.h:
+
+../third_party/ChibiOS/os/kernel/include/chthreads.h:
+
+../third_party/ChibiOS/os/kernel/include/chdynamic.h:
+
+../third_party/ChibiOS/os/kernel/include/chregistry.h:
+
+../third_party/ChibiOS/os/kernel/include/chinline.h:
+
+../third_party/ChibiOS/os/kernel/include/chqueues.h:
+
+../third_party/ChibiOS/os/kernel/include/chstreams.h:
+
+../third_party/ChibiOS/os/kernel/include/chioch.h:
+
+../third_party/ChibiOS/os/kernel/include/chfiles.h:
+
+../third_party/ChibiOS/os/kernel/include/chdebug.h:
+
+../third_party/ChibiOS/os/hal/include/hal.h:
+
+../third_party/ChibiOS/boards/ATMEL_SAM3S_CREATOR/board.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/at91sam3.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/core_cm3.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/system_SAM3.h:
+
+halconf.h:
+
+mcuconf.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/hal_lld.h:
+
+../third_party/ChibiOS/os/hal/include/tm.h:
+
+../third_party/ChibiOS/os/hal/include/pal.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.h:
+
+../third_party/ChibiOS/os/hal/include/adc.h:
+
+../third_party/ChibiOS/os/hal/include/can.h:
+
+../third_party/ChibiOS/os/hal/include/ext.h:
+
+../third_party/ChibiOS/os/hal/include/gpt.h:
+
+../third_party/ChibiOS/os/hal/include/i2c.h:
+
+../third_party/ChibiOS/os/hal/include/icu.h:
+
+../third_party/ChibiOS/os/hal/include/mac.h:
+
+../third_party/ChibiOS/os/hal/include/pwm.h:
+
+../third_party/ChibiOS/os/hal/include/rtc.h:
+
+../third_party/ChibiOS/os/hal/include/serial.h:
+
+../third_party/ChibiOS/os/hal/platforms/AT91SAM3S/serial_lld.h:
+
+../third_party/ChibiOS/os/hal/include/sdc.h:
+
+../third_party/ChibiOS/os/hal/include/spi.h:
+
+../third_party/ChibiOS/os/hal/include/uart.h:
+
+../third_party/ChibiOS/os/hal/include/usb.h:
+
+../third_party/ChibiOS/os/hal/include/mmc_spi.h:
+
+../third_party/ChibiOS/os/hal/include/serial_usb.h:
